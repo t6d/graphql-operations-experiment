b0bbe3bd6e255258349fd32c4525d831d1bc4a66
Konstantin Tennhard <konstantin@tennhard.net>
2016-09-27 16:20:52 -0400

    GraphQL query support

--- lib/app.rb
+++ lib/app.rb
@@ -2,6 +2,69 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require 'rack/parser'
 
+require 'graphql'
+require 'smart_properties'
+require 'active_operation'
+
+class Notebook
+  include SmartProperties
+
+  property :id
+  property :title
+
+  def to_json
+    {
+      id: id,
+      title: title
+    }.to_json
+  end
+end
+
+class GraphQLOperation < ActiveOperation::Base
+  def self.call(object, arguments, context)
+    super(object: object, arguments: arguments, context: context)
+  end
+end
+
+class Notebook::Find < GraphQLOperation
+  input :object, type: :keyword
+  input :arguments, type: :keyword
+  input :context, type: :keyword
+
+  def execute
+    Notebook.new(id: arguments[:id], title: "Some randome title")
+  end
+end
+
+##
+# Schema definition
+##
+
+NotebookType = GraphQL::ObjectType.define do
+  name "Notebook"
+  description "A notebook"
+
+  field :id, types.ID
+  field :title, types.String
+end
+
+QueryType = GraphQL::ObjectType.define do
+  name "Query"
+  description "The query root for this schema"
+
+  field :notebook do
+    type NotebookType
+    argument :id, !types.ID
+    resolve Notebook::Find
+  end
+end
+
+Schema = GraphQL::Schema.define(query: QueryType)
+
+##
+# Application
+##
+
 class App < Sinatra::Base
   enable :static
   set :root, File.expand_path("../..", __FILE__)
@@ -21,6 +84,10 @@ class App < Sinatra::Base
 
   post "/api" do
     content_type "application/json"
-    "{}"
+
+    query_string = params.fetch("query", "")
+    query_variables = params.fetch("variables", {})
+
+    Schema.execute(query_string, variables: query_variables).to_json
   end
 end
